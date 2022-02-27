54b9615a25af9d4c8109c13cc7e124712767a851
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-02-27 23:55:11 +0900

    feat: implement calculator as Object value

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const player = {
-  name: "JW",
-  default: function () {
-    console.log("hello");
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b + (a % b);
+  },
+  powerof: function (a, b) {
+    return a ** b;
   },
 };
 
-player.default();
-console.log("hello");
+console.log(calculator.add(10, 3));
+console.log(calculator.minus(10, 3));
+console.log(calculator.multiply(10, 3));
+console.log(calculator.divide(10, 3));
+console.log(calculator.powerof(10, 3));
