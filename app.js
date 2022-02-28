4fe3b0235c5bb9f10cb87f33d0719d1e279fc4d5
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-02-28 23:22:30 +0900

    feat : implement webpage to change it's background and color at the same time

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ function handleMouseLeave() {
   title.style.color = "black";
 }
 
+const color = ["red", "purple", "orange", "blue", "green", "yellow"];
+let colorIdx = 0;
+
+function handleResize() {
+  colorIdx++;
+  loopIdx = colorIdx % 6;
+  document.body.style.background = color[loopIdx];
+  title.style.color = color[6 - loopIdx];
+}
+
 // title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("mouseover", handleResize);
