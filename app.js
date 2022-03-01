906bdd4f0c9b2f3e0ba4d49788cf23e01ee0b27c
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-03-01 12:40:56 +0900

    feat: implement toggle to edit classList

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 const title = document.querySelector(".hello h1");
 
-// function handleTitleClick() {
-//   console.log("Title was clicked");
-// }
+const defaultClass = title.className;
+function handleClick() {
+  // const clickedClass = "clicked";
+  // if (title.classList.contains(clickedClass)) {
+  //   title.classList.remove(clickedClass);
+  // } else {
+  //   title.classList.add(clickedClass);
+  // }
 
-function handleMouseEnter() {
-  title.style.color = "blue";
+  title.classList.toggle("clicked");
 }
-
-function handleMouseLeave() {
-  title.style.color = "black";
-}
-
-const color = ["red", "purple", "orange", "blue", "green", "yellow"];
-let colorIdx = 0;
-
-function handleResize() {
-  colorIdx++;
-  loopIdx = colorIdx % 6;
-  document.body.style.background = color[loopIdx];
-  title.style.color = color[6 - loopIdx];
-}
-
-// title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("mouseover", handleResize);
+title.addEventListener("click", handleClick);
