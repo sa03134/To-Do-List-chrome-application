6bfe27f5def1ca3b638a5ef1cd83251533dee77f
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-03-01 21:35:36 +0900

    feat: implement preventDefault at anchor

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-const title = document.querySelector(".hello h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-const defaultClass = title.className;
-function handleClick() {
-  // const clickedClass = "clicked";
-  // if (title.classList.contains(clickedClass)) {
-  //   title.classList.remove(clickedClass);
-  // } else {
-  //   title.classList.add(clickedClass);
-  // }
+function handleLink(event) {
+  event.preventDefault();
+  console.dir(event);
 
-  title.classList.toggle("clicked");
+  // document.body.classList.add("trick");
+  // document.body.style.cursor = "none";
 }
-title.addEventListener("click", handleClick);
+
+const linkAnchor = document.querySelector("a");
+linkAnchor.addEventListener("click", handleLink);
+
+const fakeCursor = document.querySelector("img");
+
+document.addEventListener("click", function (event) {
+  document.body.requestPointerLock();
+  fakeCursor.classList.toggle("display");
+});
+document.addEventListener("mousemove", function (event) {
+  console.log(event);
+  // console.log(String(event.x));
+
+  let xValue = fakeCursor.style.left;
+  xValue.replace("px", "");
+  console.log(xValue);
+  // fakeCursor.style.left += String(event.movementX) + "px";
+  // fakeCursor.style.top += String(event.clientY) + "px";
+
+  // console.log(fakeCursor.style.left);
+  // console.log(fakeCursor.style.top);
+  // console.log(event.movementX);
+  // console.log(event.movementY);
+});
+
+document.body.addEventListener("pointerlockchange", function (event) {
+  if (document.pointerLockElement === document.body) {
+  }
+});
