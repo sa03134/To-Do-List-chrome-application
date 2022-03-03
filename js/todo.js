dcd9c14f70e9c28b0c15632a9bd9d49b20a2c0be
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-03-03 11:45:42 +0900

    feat: implement todoList

--- js/todo.js
+++ js/todo.js
@@ -11,19 +11,30 @@ function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+// function idFilter(item, id) {
+//   return item.id !== id;
+// }
+
+function checkItem(event) {}
+
 function deleteToDo(event) {
   const li = event.target.parentNode;
-  console.dir(event.target);
+  const id = li.id;
+  toDos = toDos.filter((item) => {
+    return JSON.stringify(item.id) !== id;
+  });
+  saveToDos();
+  console.log(toDos);
   li.remove();
   // localStorage.removeItem();
   // const todoNow = localStorage.getItem(TODOS_KEY);
-  // console.log(todoNow);
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   li.appendChild(span);
   li.appendChild(button);
@@ -39,8 +50,12 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
